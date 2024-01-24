b95acb8c0bde47f776dd810d9d54d0e494e40f6a
Stephan Krusche <krusche@in.tum.de>
2024-01-24 14:07:13 +0100

    improve example

--- src/main/java/de/tum/in/ase/KochSnowflake.java
+++ src/main/java/de/tum/in/ase/KochSnowflake.java
@@ -19,14 +19,29 @@ public class KochSnowflake extends Application {
         Flake iterativeFlake = new Flake(startPoint, endPoint);
         iterativeFlake.snow(4);
 
-        Pane pane = new Pane();
-        for (int i = 0; i < iterativeFlake.getPoints().size() - 1; i++) {
-            Point p1 = iterativeFlake.getPoints().get(i);
-            Point p2 = iterativeFlake.getPoints().get(i + 1);
-            pane.getChildren().add(new Line(p1.getX(), 300 - p1.getY(), p2.getX(), 300 - p2.getY()));
-        }
-        primaryStage.setScene(new Scene(pane, 1000, 320));
+        // Pane for Iterative Flake
+        Pane iterativePane = drawPane(iterativeFlake);
+        primaryStage.setScene(new Scene(iterativePane, 1000, 320));
         primaryStage.setTitle("Koch Snow Flake Iterative");
         primaryStage.show();
+
+        // Pane for Recursive Flake
+        Pane recursivePane = drawPane(recursiveFlake);
+
+        // Second Stage for Iterative Flake
+        Stage secondaryStage = new Stage();
+        secondaryStage.setScene(new Scene(recursivePane, 1000, 320));
+        secondaryStage.setTitle("Koch Snow Flake Recursive");
+        secondaryStage.show();
+    }
+
+    private Pane drawPane(Flake flake) {
+        Pane pane = new Pane();
+        for (int i = 0; i < flake.getPoints().size() - 1; i++) {
+            Point p1 = flake.getPoints().get(i);
+            Point p2 = flake.getPoints().get(i + 1);
+            pane.getChildren().add(new Line(p1.getX(), 300 - p1.getY(), p2.getX(), 300 - p2.getY()));
+        }
+        return pane;
     }
 }
